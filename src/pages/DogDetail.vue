952ec325b7f407c07e5a8b1b65ac26109bc0d3af
Arthur Luiz <arthurluiz36@gmail.com>
2025-11-13 20:11:04 -0300

    nova pagina de detail

---
+++ src/pages/DogDetail.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page>
+    <div class="tw-h-full">
+      <DogForm v-model="dog" @dog-emited="handleSave" />
+    </div>
+  </q-page>
+</template>
+<script setup>
+import { useRoute } from "vue-router";
+import { onBeforeMount, ref } from "vue";
+import DogForm from "../components/DogForm.vue";
+import { useHeader } from "../stores/header";
+
+const route = useRoute();
+const headerStore = useHeader();
+
+const dog = defineModel({
+  type: Object,
+  default: () => ({
+    name: "",
+    age: "",
+    birthday: "",
+    gender: "M",
+    size: "",
+    breed: "",
+    instagram: "",
+    owner: "",
+    health: {
+      has_vet: false,
+      vet_name: "",
+      vet_phone: "",
+      castrated: false,
+      in_heat: false,
+      chronic_disease: false,
+      disease_description: "",
+      allergies: "",
+      special_recommendations: "",
+    },
+  }),
+});
+
+const handleSave = (event) => {
+  console.log(event);
+};
+
+onBeforeMount(() => {
+  headerStore.title = "Cachorrinho detail";
+});
+</script>
