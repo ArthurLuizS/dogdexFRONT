95cc67c6b00846df41d5e359ac1ca36eafe4746f
Arthur Luiz <arthurluiz36@gmail.com>
2025-11-19 20:01:36 -0300

    Remodelagem de formulario para registro de dogs

--- src/components/DogForm.vue
+++ src/components/DogForm.vue
@@ -1,8 +1,23 @@
 <template>
   <q-form @submit.prevent="saveDog()" class="tw-h-full">
     <q-scroll-area class="tw-h-full tw-w-full">
-      <div class="text-h6 q-mb-sm">Dados do Cachorro</div>
-      <div class="tw-grid tw-grid-cols-3 tw-gap-3">
+      <div class="q-mb-sm">
+        <span class="text-h6"> Dados do Cachorro </span>
+        <q-icon
+          :name="expand.dog ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          class="tw-cursor-pointer"
+          size="sm"
+          @click="
+            {
+              if (route.params.id) expand.dog = !expand.dog;
+            }
+          "
+        />
+      </div>
+      <div
+        class="tw-grid tw-grid-cols-3 tw-gap-3"
+        :class="expand.dog ? '' : 'tw-hidden'"
+      >
         <q-input
           v-model="dog.name"
           label="Nome"
@@ -13,7 +28,7 @@
         />
         <q-input v-model="dog.age" label="Idade" dense rounded outlined />
         <q-input
-          v-model="dog.birthday"
+          v-model="dog.birth_date"
           label="Data de Nascimento"
           type="date"
           dense
@@ -59,52 +74,130 @@
         />
       </div>
       <q-separator spaced />
-      <div class="tw-flex tw-flex-col">Tutor</div>
+
+      <div>
+        <span class="text-h6"> Tutor </span>
+        <q-icon
+          :name="expand.dog ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          class="tw-cursor-pointer"
+          size="sm"
+          @click="
+            {
+              if (route.params.id) expand.owner = !expand.owner;
+            }
+          "
+        />
+      </div>
+      <div
+        class="tw-grid tw-grid-cols-3 tw-gap-3"
+        :class="expand.owner ? '' : 'tw-hidden'"
+      >
+        <q-input
+          v-model="dog.owner.name"
+          label="Nome"
+          dense
+          rounded
+          outlined
+          :rules="[(val) => !!val || 'Campo Obrigatório']"
+        />
+
+        <q-input
+          v-model="dog.owner.phone"
+          label="WhatsApp"
+          dense
+          rounded
+          outlined
+          :rules="[(val) => !!val || 'Campo Obrigatório']"
+          mask="(##) #####-####"
+        />
+
+        <q-input
+          v-model="dog.owner.cpf"
+          label="CPF"
+          dense
+          rounded
+          outlined
+          :rules="[(val) => !!val || 'Campo Obrigatório']"
+        />
+
+        <q-input
+          v-model="dog.owner.email"
+          label="email"
+          dense
+          rounded
+          outlined
+          :rules="[(val) => !!val || 'Campo Obrigatório']"
+        />
+
+        <q-input
+          v-model="dog.owner.address"
+          label="Endereço"
+          dense
+          rounded
+          outlined
+        />
+
+        <q-input
+          v-model="dog.owner.district"
+          label="Bairro"
+          dense
+          rounded
+          outlined
+        />
+      </div>
+
       <q-separator spaced />
       <div class="text-h6 q-mb-sm">Saúde</div>
       <div class="tw-flex tw-flex-col">
-        <q-toggle v-model="dog.health.chronic_disease" label="Possui doença?" />
-        <q-input
-          v-if="dog.health.chronic_disease"
-          v-model="dog.health.disease_description"
-          label="Descrição da doença"
-          outlined
-          dense
-          rounded
-          :rules="[(val) => !!val || 'Necessário Descrição']"
-        />
-        <q-toggle v-model="dog.health.allergies" label="Possui alergias?" />
-        <q-input
-          v-if="dog.health.allergies"
-          v-model="dog.health.allergies"
-          label="Descrição das alergias"
-          outlined
-          dense
-          rounded
-          :rules="[(val) => !!val || 'Necessário Descrição']"
-        />
-        <q-toggle v-model="dog.health.has_vet" label="Possui Veterinário?" />
-
-        <q-input
-          v-if="dog.health.has_vet"
-          v-model="dog.health.vet_name"
-          label="Nome do Veterinário"
-          outlined
-          dense
-          rounded
-          :rules="[(val) => !!val || 'Campo Obrigatório']"
-        />
-        <q-input
-          v-if="dog.health.has_vet"
-          v-model="dog.health.vet_phone"
-          label="Telefone do Veterinário"
-          outlined
-          dense
-          rounded
-          :rules="[(val) => !!val || 'Campo Obrigatório']"
-        />
-        <q-toggle v-model="dog.health.castrated" label="Castrado" />
-        <q-toggle v-model="dog.health.in_heat" label="No cio" />
+        <div>
+          <q-toggle v-model="dog.health.castrated" label="Castrado" />
+          <q-toggle v-model="dog.health.in_heat" label="No cio" />
+          <q-toggle v-model="dog.health.has_vet" label="Possui Veterinário?" />
+          <q-toggle
+            v-model="dog.health.chronic_disease"
+            label="Doença/Alergias?"
+          />
+        </div>
+        <div class="tw-grid tw-grid-cols-3 tw-gap-3">
+          <q-input
+            v-if="dog.health.has_vet"
+            v-model="dog.health.vet_name"
+            label="Nome do Veterinário"
+            outlined
+            dense
+            rounded
+            :rules="[(val) => !!val || 'Campo Obrigatório']"
+          />
+          <q-input
+            v-if="dog.health.has_vet"
+            v-model="dog.health.vet_phone"
+            label="Telefone do Veterinário"
+            outlined
+            dense
+            rounded
+            :rules="[(val) => !!val || 'Campo Obrigatório']"
+          />
+        </div>
+        <div>
+          <q-input
+            v-if="dog.health.chronic_disease"
+            v-model="dog.health.disease_description"
+            label="Descrição da doença"
+            outlined
+            dense
+            rounded
+            :rules="[(val) => !!val || 'Necessário Descrição']"
+          />
+          <q-input
+            v-if="dog.health.chronic_disease"
+            v-model="dog.health.allergies"
+            label="Descrição das alergias"
+            outlined
+            dense
+            rounded
+            :rules="[(val) => !!val || 'Necessário Descrição']"
+          />
+        </div>
       </div>
     </q-scroll-area>
     <div class="tw-w-full tw-flex tw-justify-end">
@@ -114,44 +207,41 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits } from "vue";
-import { Health } from "../types/health";
+import { defineEmits, ref } from "vue";
+import { Dog } from "../types/index";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
 
 const emits = defineEmits(["dogEmited"]);
 
-interface Dog {
-  name: string;
-  age: string;
-  birthday: string;
-  gender: string;
-  size: string;
-  breed: string;
-  instagram: string;
-  owner: string;
-  health: Health;
-}
-
-const dog = defineModel({
-  type: Object,
+const dog = defineModel<Dog>({
   default: () => ({
     name: "",
     age: "",
-    birthday: "",
+    birth_date: "",
     gender: "M",
     size: "",
     breed: "",
     instagram: "",
-    owner: "",
     health: {
       has_vet: false,
       vet_name: "",
       vet_phone: "",
-      castrated: false,
+      castrated: true,
       in_heat: false,
       chronic_disease: false,
-      disease_description: "",
       allergies: "",
+      disease_description: "",
       special_recommendations: "",
+    },
+    owner: {
+      name: "",
+      phone: "",
+      cpf: "",
+      email: "",
+      address: "",
+      district: "",
     },
   }),
 });
@@ -181,6 +271,12 @@ const genders = [
     value: "F",
   },
 ];
+
+const expand = ref({
+  dog: true,
+  owner: true,
+  health: true,
+});
 
 const saveDog = () => {
   emits("dogEmited", { ...dog.value });
--- src/pages/DogRegistration.vue
+++ src/pages/DogRegistration.vue
@@ -1,165 +1,7 @@
 <template>
   <q-page>
     <div class="q-pa-md tw-border tw-h-full tw-pb-16">
-      <DogForm />
-      <!-- <q-form @submit="() => console.log(dog)" class="tw-h-full">
-        <q-scroll-area class="tw-h-full tw-w-full">
-          <div class="text-h6 q-mb-sm">Dados do Cachorro</div>
-          <div class="tw-grid tw-grid-cols-3 tw-gap-3">
-            <q-input
-              v-model="dog.name"
-              label="Nome"
-              dense
-              rounded
-              outlined
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-input v-model="dog.age" label="Idade" dense rounded outlined />
-            <q-input
-              v-model="dog.birthday"
-              label="Data de Nascimento"
-              type="date"
-              dense
-              rounded
-              outlined
-            />
-            <q-select
-              v-model="dog.gender"
-              :options="genders"
-              label="Gênero"
-              dense
-              rounded
-              outlined
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-select
-              v-model="dog.size"
-              :options="sizes"
-              label="Tamanho"
-              dense
-              rounded
-              outlined
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-input
-              v-model="dog.breed"
-              label="Raça"
-              dense
-              rounded
-              outlined
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-input
-              v-model="dog.instagram"
-              label="Instagram"
-              dense
-              rounded
-              outlined
-            />
-          </div>
-
-          <q-separator spaced />
-          <div class="text-h6 q-mb-sm">Dono</div>
-          <div class="tw-grid tw-grid-cols-3 tw-gap-3">
-            <q-input
-              v-model="dog.owner.name"
-              label="Nome"
-              outlined
-              dense
-              rounded
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-input
-              v-model="dog.owner.addres"
-              label="Endereço"
-              outlined
-              dense
-              rounded
-            />
-            <q-input
-              v-model="dog.owner.district"
-              label="Bairro"
-              outlined
-              dense
-              rounded
-            />
-            <q-input
-              v-model="dog.owner.cpf"
-              label="CPF"
-              outlined
-              dense
-              rounded
-              mask="###.###.###-##"
-              unmasked-value
-            />
-            <q-input
-              v-model="dog.owner.phone"
-              label="Telefone"
-              outlined
-              dense
-              rounded
-              mask="(##)#####-####"
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-          </div>
-
-          <q-separator spaced />
-          <div class="text-h6 q-mb-sm">Saúde</div>
-          <div class="tw-flex tw-flex-col">
-            <q-toggle v-model="dog.health.disease" label="Possui doença?" />
-            <q-input
-              v-if="dog.health.disease"
-              v-model="dog.health.diseaseDesc"
-              label="Descrição da doença"
-              outlined
-              dense
-              rounded
-              :rules="[(val) => !!val || 'Necessário Descrição']"
-            />
-            <q-toggle v-model="dog.health.allergies" label="Possui alergias?" />
-            <q-input
-              v-if="dog.health.allergies"
-              v-model="dog.health.allergiesDesc"
-              label="Descrição das alergias"
-              outlined
-              dense
-              rounded
-              :rules="[(val) => !!val || 'Necessário Descrição']"
-            />
-            <q-toggle v-model="dog.health.HasVet" label="Possui Veterinário?" />
-
-            <q-input
-              v-if="dog.health.HasVet"
-              v-model="dog.health.vet.name"
-              label="Nome do Veterinário"
-              outlined
-              dense
-              rounded
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-input
-              v-if="dog.health.HasVet"
-              v-model="dog.health.vet.phone"
-              label="Telefone do Veterinário"
-              outlined
-              dense
-              rounded
-              :rules="[(val) => !!val || 'Campo Obrigatório']"
-            />
-            <q-toggle v-model="dog.health.castrated" label="Castrado" />
-            <q-toggle v-model="dog.health.inHeart" label="No cio" />
-          </div>
-        </q-scroll-area>
-        <div class="tw-w-full tw-flex tw-justify-end">
-          <q-btn
-            type="submit"
-            label="Salvar"
-            color="primary"
-            class=""
-            rounded
-          />
-        </div>
-      </q-form> -->
+      <DogForm v-model="dog" @dog-emited="handleSave" />
     </div>
   </q-page>
 </template>
@@ -167,84 +9,41 @@
 <script setup lang="ts">
 import { ref, onBeforeMount } from "vue";
 import DogForm from "../components/DogForm.vue";
-import { useOwner } from "../stores/owners";
-import { useDogs } from "../stores/dogs";
-
-interface Owner {
-  name: string;
-  addres: string;
-  district: string;
-  cpf: string;
-  phone: string;
-}
-
-interface Veterinarian {
-  name: string;
-  phone: string;
-}
-
-interface health {
-  HasVet: boolean;
-  vet: Veterinarian | null;
-  castrated: boolean;
-  inHeart: boolean;
-  disease: boolean;
-  diseaseDesc: string | null;
-  allergies: boolean | null;
-  allergiesDesc: string | null;
-}
-
-interface Dog {
-  name: string;
-  age: string;
-  birthday: string;
-  sexo: string;
-  gender: string;
-  size: string;
-  breed: string;
-  instagram: string;
-  owner: Owner;
-  health: health;
-}
+import { Dog } from "../types/index";
 
 const dog = ref<Dog>({
   name: "",
-  age: "",
-  birthday: "",
-  sexo: "",
-  gender: "",
-  size: "",
+  age: 0,
+  birth_date: "",
+  gender: "M",
+  size: "P",
   breed: "",
   instagram: "",
+  health: {
+    has_vet: false,
+    vet_name: "",
+    vet_phone: "",
+    castrated: true,
+    in_heat: false,
+    chronic_disease: false,
+    allergies: "",
+    disease_description: "",
+    special_recommendations: "",
+  },
   owner: {
     name: "",
-    addres: "",
-    district: "",
-    cpf: "",
     phone: "",
+    cpf: "",
+    email: "",
+    address: "",
+    district: "",
   },
-  health: {
-    HasVet: false,
-    vet: {
-      name: "",
-      phone: "",
-    },
-    castrated: false,
-    inHeart: false,
-    disease: false,
-    diseaseDesc: null,
-    allergies: false,
-    allergiesDesc: null,
-  },
+  is_active: true,
 });
 
-const sizes = ["Pequeno", "Médio", "Grande"];
-const genders = ["Macho", "Fêmea"];
+const handleSave = (dog: Dog) => {
+  console.log(dog);
+};
 
-const ownerStore = useOwner();
-const dogStore = useDogs();
-onBeforeMount(() => {
-  ownerStore.getAllOwners();
-  dogStore.getAllDogs();
-});
+onBeforeMount(() => {});
 </script>
